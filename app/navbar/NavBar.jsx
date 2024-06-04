aa797720205d67097650d5b02bba1e51fca875d1
Kaushiki Bhattacharyya <141013087+itsKaushiki@users.noreply.github.com>
2024-06-04 10:12:43 +0530

    Update NavBar.jsx with SignIn and SignUp

--- app/navbar/NavBar.jsx
+++ app/navbar/NavBar.jsx
@@ -2,17 +2,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useState } from 'react';
-import {RegisterLink, LoginLink,LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs";
+import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+
 function NavBar() {
   const [navbar, setNavbar] = useState(false);
-  const {user, isAuthenticated} = useKindeBrowserClient();
+  const { user, isAuthenticated } = useKindeBrowserClient();
+
+  const handleLinkClick = () => {
+    if (navbar) {
+      setNavbar(false);
+    }
+  };
+
   return (
     <div>
-      <nav className="w-full bg-slate-100 fixed top-0 left-0 right-0 z-10 font-etica">
-        <div className="justify-between shadow px-4 mx-auto md:items-center md:flex md:px-8">
+      <nav className="w-full bg-custom-green fixed top-0 left-0 right-0 z-10 font-etica">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div className="flex items-center justify-between py-3 md:py-5 w-full">
-          
             <div className="md:hidden">
               <button
                 className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
@@ -31,13 +38,13 @@ function NavBar() {
                 )}
               </button>
             </div>
-    
+
             <Link href="/">
-              <h2 className="lg:text-4xl text-2xl text-black font-bold lg:ml-4">NOTESHAALA</h2>
+              <h2 className="text-4xl text-black font-bold ml-4">NOTESHAALA</h2>
             </Link>
-           
+
             {navbar && (
-              <div className="mr-4 flex items-center">
+              <div className="mr-4 flex items-center md:hidden">
                 <Link href="/user-profile">
                   <Image
                     src="/user.svg"
@@ -57,32 +64,27 @@ function NavBar() {
               } ${navbar ? 'text-center justify-center' : ''}`}
             >
               <ul className={`h-screen md:h-auto items-center ${navbar ? 'justify-center flex-col' : 'md:flex'}`}>
-                {isAuthenticated&&(
-                   <li className={`pb-2 py-2 md:px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
-                    navbar ? 'text-2xl my-4' : 'text-xl'
-                  }`}>
-                       
-                    <Link href="#profiles" onClick={() => setNavbar(!navbar)}>
-                      <div className="flex items-center justify-center">
-                        <Image
-                          src="/user.svg"
-                          width={20}
-                          height={20}
-                          alt="User"
-                          className="cursor-pointer mr-2"
-                        />
-                        {user.given_name}
-                      </div>
-                    </Link>
-                  </li>
-                )
-                }
-               
+                <li className={`pb-2 py-2 md:px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
+                  navbar ? 'text-2xl my-4' : 'text-xl'
+                }`}>
+                  <Link href="#profiles" onClick={handleLinkClick}>
+                    <div className="flex items-center justify-center">
+                      <Image
+                        src="/user.svg"
+                        width={20}
+                        height={20}
+                        alt="User"
+                        className="cursor-pointer mr-2"
+                      />
+                      Profile
+                    </div>
+                  </Link>
+                </li>
                 <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
                   navbar ? 'text-2xl my-4' : 'text-xl'
                 }`}>
-                  <Link href="#notes" onClick={() => setNavbar(!navbar)}>
-                    <div className="flex items-center justify-center">
+                  <Link href="#notes" onClick={handleLinkClick}>
+                    <div className="flex items-center justify-center space-x-4">
                       <Image
                         src="/notes.svg"
                         width={20}
@@ -97,8 +99,8 @@ function NavBar() {
                 <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
                   navbar ? 'text-2xl my-4' : 'text-xl'
                 }`}>
-                  <Link href="#reviews" onClick={() => setNavbar(!navbar)}>
-                    <div className="flex items-center justify-center">
+                  <Link href="#reviews" onClick={handleLinkClick}>
+                    <div className="flex items-center justify-center space-x-4">
                       <Image
                         src="/smile.svg"
                         width={20}
@@ -113,8 +115,8 @@ function NavBar() {
                 <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
                   navbar ? 'text-2xl my-4' : 'text-xl'
                 }`}>
-                  <Link href="#uploads" onClick={() => setNavbar(!navbar)}>
-                    <div className="flex items-center justify-center">
+                  <Link href="#uploads" onClick={handleLinkClick}>
+                    <div className="flex items-center justify-center space-x-4">
                       <Image
                         src="/upload.svg"
                         width={20}
@@ -129,8 +131,8 @@ function NavBar() {
                 <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
                   navbar ? 'text-2xl my-4' : 'text-xl'
                 }`}>
-                  <Link href="#contacts" onClick={() => setNavbar(!navbar)}>
-                    <div className="flex items-center justify-center">
+                  <Link href="#contacts" onClick={handleLinkClick}>
+                    <div className="flex items-center justify-center space-x-4">
                       <Image
                         src="/arrows.svg"
                         width={20}
@@ -141,45 +143,36 @@ function NavBar() {
                       Contact
                     </div>
                   </Link>
-                  
                 </li>
-               
-                    <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
-                      navbar ? 'text-2xl my-4' : 'text-xl'
-                    }`}>
-                   {
-                  !isAuthenticated&&(
-                      <LoginLink>
-                        <div className="flex items-center justify-center border-[#29b5f6] py-2 px-4 rounded-lg border-[3px] ">
-                        SignIn
-                        </div>
-                        </LoginLink>
-                      )
-                      }
-                      {
-                  isAuthenticated&&(
-                      <LogoutLink>
-                        <div className="flex items-center justify-center bg-gradient-to-r from-[#29b5f6] to-[#67c5f1d5] py-2 px-4 rounded-lg">
-                        LogOut
-                        </div>
-                        </LogoutLink>
-                      )
-                      }
-                    </li>
-                    
-                  
-                {
-                  !isAuthenticated&&(
                 <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
-                      navbar ? 'text-2xl my-4' : 'text-xl'
-                    }`}>
-                      <RegisterLink>
-                        <div className="flex items-center justify-center bg-gradient-to-r from-[#29b5f6] to-[#67c5f1d5] py-2 px-2 rounded-lg ">
-                        SignUp
-                        </div>
-                        </RegisterLink>
-                    </li>
+                  navbar ? 'text-2xl my-4' : 'text-xl'
+                }`}>
+                  {!isAuthenticated && (
+                    <LoginLink>
+                      <div className="flex items-center justify-center border-[#29b5f6] py-2 px-4 rounded-lg border-[3px] hover:bg-blue-200">
+                        SignIn
+                      </div>
+                    </LoginLink>
                   )}
+                  {isAuthenticated && (
+                    <LogoutLink>
+                      <div className="flex items-center justify-center bg-gradient-to-r from-[#29b5f6] to-[#67c5f1d5] py-2 px-4 rounded-lg hover:bg-blue-300">
+                        LogOut
+                      </div>
+                    </LogoutLink>
+                  )}
+                </li>
+                {!isAuthenticated && (
+                  <li className={`pb-2 py-2 px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-200 border-gray-800 md:hover:text-black ${
+                    navbar ? 'text-2xl my-4' : 'text-xl'
+                  }`}>
+                    <RegisterLink>
+                      <div className="flex items-center justify-center bg-gradient-to-r from-[#29b5f6] to-[#67c5f1d5] py-2 px-2 rounded-lg hover:bg-blue-300">
+                        SignUp
+                      </div>
+                    </RegisterLink>
+                  </li>
+                )}
               </ul>
             </div>
           </div>
