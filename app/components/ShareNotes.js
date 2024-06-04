45a275f1116f06e37ee70fdf156fb15aa7f8cb3b
Premraj18 <premrajprasad18@gmail.com>
2024-06-04 09:16:45 +0530

    abc

--- app/components/ShareNotes.js
+++ app/components/ShareNotes.js
@@ -10,7 +10,7 @@ const ShareNotes = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-5 text-center mb-10'>
             <h2 className='text-3xl lg:text-4xl font-semibold'>SHARE YOUR NOTES</h2>
-            <p className='lg:text-xl lg:my-5'>Help your fellow mates with by sharing your valuable notes</p>
+            <p className='lg:text-xl lg:my-5 px-2'>Help your fellow mates with by sharing your valuable notes</p>
             {isAuthenticated && (<Link href='/uploadnotes'>
                 <div className='border-white border-[2px] rounded-lg px-7 py-2 text-lg bg-[#29b5f6d5] hover:bg-[#29b5f686] hover:scale-[1.02]'>Upload Notes</div>
             </Link>)}
--- app/navbar/NavBar.jsx
+++ app/navbar/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
   return (
     <div>
       <nav className="w-full bg-slate-100 fixed top-0 left-0 right-0 z-10 font-etica">
-        <div className="justify-between  px-4 mx-auto md:items-center md:flex md:px-8">
+        <div className="justify-between shadow px-4 mx-auto md:items-center md:flex md:px-8">
           <div className="flex items-center justify-between py-3 md:py-5 w-full">
           
             <div className="md:hidden">
